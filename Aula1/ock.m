e6a1856cae30b79480cf355fddd058c8de7a1562
Henrique Hyonemoto <91375748+HenriqueHyonemoto@users.noreply.github.com>
2024-09-04 10:58:02 -0300

    Add files via upload

---
+++ Aula1/ock.m
@@ -0,0 +1,20 @@
+pkg load image;
+clc;
+
+img = imread('C:\Users\Veterano\Documents\H_Topics\Aula1\lena512.bmp');
+
+imgBkQuad = img;
+
+for col = 1:35
+  for lin = 1:55
+    imgBkQuad(col,lin) = 0;
+  endfor
+endfor
+
+figure(1),imshow(img)
+figure(2),imshow(imgBkQuad)
+
+
+
+
+
