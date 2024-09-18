d67d7661a3af5e7b66a4ead68712f525ba8a1d53
HenriqueHyonemoto <henrique.hyonemoto@aluno.ifsp.edu.br>
2024-09-18 10:58:45 -0300

    comment

--- Aula2/6_cortar_imagen.m
+++ Aula2/6_cortar_imagen.m
@@ -2,7 +2,7 @@ pkg load image;
 clc;
 
 a = imread('C:\Users\Veterano\Documents\H_Topicos\Aula2\lena512.bmp');
-b = imcrop(a,[100,100,180,180]);
+b = imcrop(a,[100,100,180,180]); #100,100 são locais que vao começar o corte posição (linha 100, coluna 100) e (linha 180, coluna 180) isso corta um quadrado no centro da imagem
 
 figure(1), subplot(2,1,1),colormap(gray),imshow(a);
 figure(1), subplot(2,1,2),colormap(gray),imshow(b);
