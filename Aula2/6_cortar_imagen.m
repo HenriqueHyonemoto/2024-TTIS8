74aa95b7e47d65354e512ecb93d69038df05981a
HenriqueHyonemoto <henrique.hyonemoto@aluno.ifsp.edu.br>
2024-09-18 10:57:07 -0300

    lena512

--- Aula2/6_cortar_imagen.m
+++ Aula2/6_cortar_imagen.m
@@ -1,7 +1,7 @@
 pkg load image;
 clc;
 
-a = imread('C:\Users\Veterano\Documents\H_Topicos\Aula2\lena128.bmp');
+a = imread('C:\Users\Veterano\Documents\H_Topicos\Aula2\lena512.bmp');
 b = imcrop(a,[100,100,180,180]);
 
 figure(1), subplot(2,1,1),colormap(gray),imshow(a);
