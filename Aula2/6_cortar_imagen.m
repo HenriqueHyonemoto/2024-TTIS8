364ee48c30b568e1c652d9d41a1985e4435db931
HenriqueHyonemoto <henrique.hyonemoto@aluno.ifsp.edu.br>
2024-09-18 10:55:36 -0300

    cortar

---
+++ Aula2/6_cortar_imagen.m
@@ -0,0 +1,18 @@
+pkg load image;
+clc;
+
+a = imread('C:\Users\Veterano\Documents\H_Topicos\Aula2\lena128.bmp');
+b = imcrop(a,[100,100,180,180]);
+
+figure(1), subplot(2,1,1),colormap(gray),imshow(a);
+figure(1), subplot(2,1,2),colormap(gray),imshow(b);
+
+
+
+
+
+
+
+
+
+
