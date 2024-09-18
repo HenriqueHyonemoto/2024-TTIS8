276ec9b5e8eebafb2826d5c507a0a4c57bc8afc1
HenriqueHyonemoto <henrique.hyonemoto@aluno.ifsp.edu.br>
2024-09-18 10:28:17 -0300

    comentario

--- Aula2/4_equalizar histograma.m
+++ Aula2/4_equalizar histograma.m
@@ -3,9 +3,14 @@ clc;
 
 imgEsc = imread('C:\Users\Veterano\Documents\H_Topicos\Aula2\ImagemTesteEscuro.bmp');
 
+#imgEsc = rgb2gray(img); caso a imagem não esteja em tons de cinza.
+
+
 imgEq = histeq(imgEsc); #função de equalizar o histograma
 imgEq = imgEq*255;
 imgEq= uint8(imgEq);
+
+
 
 figure(1),colormap(gray),imshow(imgEsc), title('Imagem Escura');
 figure(2),colormap(gray),imshow(imgEq), title('Imagem Equalizada');
