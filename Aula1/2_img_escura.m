9d99e8d420df4fce82688ea3ad7bd35f3c4b0782
HenriqueHyonemoto <henrique.hyonemoto@aluno.ifsp.edu.br>
2024-09-18 08:33:05 -0300

    ex anteriores

---
+++ Aula1/2_img_escura.m
@@ -0,0 +1,16 @@
+pkg load image;
+clc;
+
+img = imread('C:\Users\Veterano\Documents\H_Topics\Aula1\lena512.bmp');
+
+imgBkQuad = img;
+
+for col = 1:512
+  for lin = 1:512
+    imgBkQuad(col,lin) -= 100;
+  endfor
+endfor
+
+figure(1),imshow(img)
+figure(2),imshow(imgBkQuad)
+
---
+++ Aula1/tres_binario.m
@@ -0,0 +1,21 @@
+pkg load image;
+clc;
+
+img = imread('C:\Users\Veterano\Documents\H_Topics\Aula1\lena512.bmp');
+
+imgBin = img;
+
+[M, N] = size(img);
+
+for i = 1:M
+  for j = 1:N
+    if img(i,j) >= 127
+      imgBin(i,j) = 255;
+    else
+      imgBin(i,j) = 0;
+    endif
+  endfor
+endfor
+
+figure(1),imshow(img);
+figure(2),imshow(imgBin);
