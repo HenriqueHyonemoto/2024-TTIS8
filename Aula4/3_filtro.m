95c825d3af1397b06c588b2e073ef1d26cdfaa57
HenriqueHyonemoto <henrique.hyonemoto@aluno.ifsp.edu.br>
2024-10-02 10:50:05 -0300

    sss

---
+++ Aula4/3_filtro.m
@@ -0,0 +1,24 @@
+pkg load image;
+clc;
+
+img = imread('cameraman128.bmp');
+
+img = imnoise(img, 'Salt & Pepper');
+
+filtro = [1,1,1;1,1,1;1,1,1];
+filtro = filtro/9;
+
+imgSuavizada = conv2(filtro, img);
+imgSuavizada = uint8(imgSuavizada);
+
+figure(1),subplot(1,2,1),imshow(img);
+figure(1),subplot(1,2,2),imshow(imgSuavizada);
+
+
+
+
+
+
+
+
+
---
+++ Aula4/5_filtros.m
@@ -0,0 +1,26 @@
+pkg load image;
+clc;
+
+
+filtro1 = [-4 -4 -4;
+             4 2;
+            -1 -2 -1]; #2 barras horizontais
+
+s = [-1 2 -1; -1 2 -1; -1 2 -1]; #2 barras verticais
+img = zeros(10,10);
+
+img(3:7,3:7)=1;
+
+b = conv2(s,img);
+
+figure(1),subplot(1,2,1),imshow(img);
+figure(1),subplot(1,2,2),imshow(b);
+
+
+
+
+
+
+
+
+
